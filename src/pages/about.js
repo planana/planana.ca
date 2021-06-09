a576baea25cc3d7bdb422087d0aaf5f269bdcfe6
Annette Lau <annette1@ualberta.ca>
2021-06-09 00:46:13 -0600

    fixed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
 import React from "react";
-import styles from "./about.module.css";
+import "../css/custom.css";
+import Layout from "@theme/Layout";
 
-export default function About() {
+export default function AboutPage() {
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <Layout title="Our Story" description="Planana's mission or something">
       <div className="container">
-        <p className="hero__subtitle">The About Page</p>
+        <p className="title text--center slide-in-left"> PLANANA AAAA</p>
+        <div
+          className="headerLine centered slide-in-left"
+          style={{ animationDelay: "0.5s" }}
+        />
+        <div className="text-box centered">
+          <p
+            className="body text--center fadeIn"
+            style={{ animationDelay: "1s" }}
+          >
+            We are planananananaaanananananananaanananannanaaaaaaaaaaaaaaaaaaaa
+          </p>
+        </div>
+        <div className="fadeIn" style={{ animationDelay: "1.5s" }}></div>
       </div>
-    </header>
+    </Layout>
   );
 }
