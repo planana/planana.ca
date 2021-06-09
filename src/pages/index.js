b5970ed39838151af978415ae1290ac1e5fb7492
Annette Lau <annette1@ualberta.ca>
2021-06-09 00:01:00 -0600

    remove unnecessary things

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
-import HomepageFeatures from "../components/HomepageFeatures";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -16,14 +14,14 @@ function HomepageHeader() {
           The mon
           <b>KEY</b> behind event planning.
         </p>
-        <div className={styles.buttons}>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
             Docusaurus Tutorial - 5min ⏱️
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
